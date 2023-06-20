4d1d7e788af3c048d8d26ab4a33b63aa2317ae67
alfiankan <alfiankan19@gmail.com>
2023-06-20 22:21:39 +0700

    feat(redirect): home to board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = '/challenge/1'
+  }, [0])
   return <>Redirecting</>
 }
